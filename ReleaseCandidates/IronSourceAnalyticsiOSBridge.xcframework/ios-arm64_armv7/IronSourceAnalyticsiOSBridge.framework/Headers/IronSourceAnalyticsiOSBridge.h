b161c8a1ea117d09d64f9cc1a3b155354f1176a9
jenkins-build-machine <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>
2022-03-07 13:08:42 +0200

    Release '0.1.0.0'

---
+++ ReleaseCandidates/IronSourceAnalyticsiOSBridge.xcframework/ios-arm64_armv7/IronSourceAnalyticsiOSBridge.framework/Headers/IronSourceAnalyticsiOSBridge.h
@@ -0,0 +1,12 @@
+//
+//  Analytics_iOSBridge.h
+//  Analytics-iOSBridge
+//
+//  Created by Bar David on 15/06/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IronSourceAnalyticsiOSBridge : NSObject
+
+@end
